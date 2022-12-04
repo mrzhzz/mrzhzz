b39e475aaae3db4a204fa5039e657943855bb9fe
Mrz <1981882866@qq.com>
2022-12-04 21:09:56 +0800

    The first time use git

---
+++ com.test/src/Test1.java
@@ -0,0 +1,6 @@
+public class Test1 {
+    Test1(){}
+    public void hello(){
+        System.out.println("hello,git");
+    }
+}
---
+++ com.test/src/TestMain.java
@@ -0,0 +1,6 @@
+public class TestMain {
+    public static void main(String[] args) {
+        Test1 t1 = new Test1();
+        t1.hello();
+    }
+}
