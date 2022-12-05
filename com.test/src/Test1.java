ad66d3526d07b9ab47394e6707dc371050a7a41c
Mrz <1981882866@qq.com>
2022-12-05 14:51:30 +0800

    ArrayTest1 2

--- com.test/src/Test1.java
+++ com.test/src/Test1.java
@@ -1,6 +1,40 @@
 public class Test1 {
     Test1(){}
     public void hello(){
+
         System.out.println("hello,git");
     }
+
+
+    public void testArray(){
+
+        char c[] = new char[26];
+        c[0] = 'A';
+
+        //
+        for(int i=1;i<26;i++){
+            c[i] = (char)(c[i-1] +1);
+        }
+        /*for(int i=1;i<c.length;i++){
+            c[i] = (char)('A' + i); //char + int = int ,强转为char
+        }*/
+
+
+        for (int i=0;i<26;i++){
+            System.out.println(c[i]);
+        }
+    }
+
+
+    public void testArray2(int arr[]){
+        int max = arr[0];
+        for (int i=1;i<arr.length;i++){
+            if(max<arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+
 }
--- com.test/src/TestMain.java
+++ com.test/src/TestMain.java
@@ -1,6 +1,23 @@
+import java.util.Random;
+
 public class TestMain {
     public static void main(String[] args) {
         Test1 t1 = new Test1();
-        t1.hello();
+        Random ra = new Random();
+
+        /*t1.hello();*/
+
+        /*t1.testArray();*/
+
+        /*int array[] = new int[10];
+        for(int i=0;i<array.length;i++){
+            array[i] = ra.nextInt(100);
+        }
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+        t1.testArray2(array);*/
+
+
     }
 }
